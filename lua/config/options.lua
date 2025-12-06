373fda3a1f261e91415f94c9968f887e945a1fe0
Adalberto Junior <adalberto.gmu@gmail.com>
2025-12-06 15:22:58 -0300

    change cursorlinenr, linenr and shiftwidth

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,10 +1,11 @@
 vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.relativenumber = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.signcolumn = "yes"
 
-
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#bbbbbb", bg = "#1f1f1f" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#5a5a5a", bg = "#1f1f1f" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3c3836" })
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#444444", fg = "#ffffff" })
