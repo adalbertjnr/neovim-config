cf89207d858271539d495cf35852c2b6435f731f
Adalberto Junior <adalberto.gmu@gmail.com>
2025-12-06 16:21:47 -0300

    change linenr color

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.signcolumn = "yes"
 
-vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#bbbbbb", bg = "#1f1f1f" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#5a5a5a", bg = "#1f1f1f" })
+vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#bbbbbb" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#ffffff" })
 vim.api.nvim_set_hl(0, "CursorLine", { bg = "#3c3836" })
 vim.api.nvim_set_hl(0, "YankHighlight", { bg = "#444444", fg = "#ffffff" })
