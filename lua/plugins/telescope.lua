a22736032764970dcfcefb7417d552eb7b633ef7
Adalberto Junior <adalberto.gmu@gmail.com>
2026-02-11 18:20:31 -0300

    add pickers and file ignore patterns telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,38 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.8',
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function()
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-    vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-  end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.8",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {
+		defaults = {
+			file_ignore_patterns = {
+				".git/",
+				".cache",
+				"%.o",
+				"%.a",
+				"%.out",
+				"%.class",
+				"%.pdf",
+				"%.mkv",
+				"%.mp4",
+				"%.zip",
+			},
+		},
+		pickers = {
+			find_files = {
+				hidden = true,
+			},
+		},
+		live_grep = {
+			additional_arogs = function()
+				return { "--hidden" }
+			end,
+		},
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+	end,
 }
