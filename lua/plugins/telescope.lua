ffb8a2a87cf5bcd419a9e6729f0a67d0efa7cfc6
Adalberto Junior <adalberto.gmu@gmail.com>
2026-02-14 15:45:34 -0300

    fix telescope opts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -2,6 +2,7 @@ return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim" },
+
 	opts = {
 		defaults = {
 			file_ignore_patterns = {
@@ -24,7 +25,10 @@ return {
 			},
 		},
 	},
-	config = function()
+
+	config = function(_, opts)
+		require("telescope").setup(opts)
+
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
