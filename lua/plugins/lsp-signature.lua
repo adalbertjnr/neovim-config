28d7999bf9ec7fae33f996dfc9a3060ec2c14f07
Adalberto Junior <adalberto.gmu@gmail.com>
2026-05-31 15:38:30 -0300

    lets see if it works

--- lua/plugins/lsp-signature.lua
+++ lua/plugins/lsp-signature.lua
@@ -1,6 +1,6 @@
 return {
 	"ray-x/lsp_signature.nvim",
-	event = "LspAttach",
+	event = "InsertEnter",
 	opts = {
 		handler_opts = {
 			border = "single", -- double, rounded, single, shadow, none, or a table of borders
@@ -8,4 +8,13 @@ return {
 		always_trigger = true,
 		hint_enable = false,
 	},
+
+	config = function(_, opts)
+		local exclude = { terraform = true, tf = true }
+		if exclude[vim.bo.filetype] then
+			return
+		end
+
+		require("lsp_signature").setup(opts)
+	end,
 }
